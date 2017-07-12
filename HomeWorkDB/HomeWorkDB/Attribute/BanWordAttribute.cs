84e80b1c68615647b165357849b438c9167ce2e7
Ian Chen 陳立峯 <fds26875@hotmail.com>
2017-07-12 21:12:10 +0800

    BanWord驗證加入

---
+++ HomeWorkDB/HomeWorkDB/Attribute/BanWordAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomeWorkDB.Attribute
+{
+    public sealed class BanWordAttribute : ValidationAttribute
+    {
+        private string Input { get; set; }
+        public BanWordAttribute(string input)
+        {
+
+        }
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            if (value is string)
+            {
+                //輸入值是字串才判斷
+                if (this.Input.Contains(value.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
