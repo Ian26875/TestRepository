80235d886bdec53ea1d690e6f3bf4a7ac847201c
Ian Chen 陳立峯 <fds26875@hotmail.com>
2017-07-23 20:16:03 +0800

    新增 修改密碼ViewModel

---
+++ Solution/WebApplication/ViewModels/MemberChangePasswordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.ViewModels
+{
+    /// <summary>
+    /// 修改密碼ViewModel
+    /// </summary>
+    public class MemberChangePasswordViewModel
+    {
+        [Display(Name = "舊密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        public string Password { get; set; }
+        [Display(Name = "新密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        public string NewPassword { get; set; }
+
+        [Display(Name = "新密碼確認")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        [Compare("NewPassword", ErrorMessage = "兩次輸入密碼不一致")]
+        public string NewPasswordCheck { get; set; }
+    }
+}
