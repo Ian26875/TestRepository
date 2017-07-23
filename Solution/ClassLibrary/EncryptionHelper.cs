f69cf20dc0e24998b38d57fc0d48cfde6ca08b2c
Ian Chen 陳立峯 <fds26875@hotmail.com>
2017-07-23 20:25:50 +0800

    新增MemberService的註冊方法,及EncryptionHelper加密的類別

---
+++ Solution/ClassLibrary/EncryptionHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public sealed class EncryptionHelper
+    {
+        public delegate string EncodingString(string source);
+        /// <summary> 
+        /// MD5加密字符串 
+        /// </summary> 
+        /// <param name="source">原字串</param> 
+        /// <returns>加密後字串</returns> 
+        public string EncodingMD5(string source)
+        {
+            MD5 md5 = MD5.Create();
+            string resultMd5 = Convert.ToBase64String(md5.ComputeHash(Encoding.Default.GetBytes(source)));
+            return resultMd5; ;
+        }
+
+
+        /// <summary>
+        /// SHA1加密字串
+        /// </summary>
+        /// <param name="source">原字串</param>
+        /// <returns>加密後字串</returns>
+        public string EncodingSHA1(string source)
+        {
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            string resultSha1 = Convert.ToBase64String(sha1.ComputeHash(Encoding.Default.GetBytes(source)));
+            return resultSha1;
+        }
+        /// <summary>
+        /// SHA384加密字串
+        /// </summary>
+        /// <param name="source">原字串</param>
+        /// <returns>加密後字串</returns>
+        public string EncodingSHA384(string source)
+        {
+            SHA384 sha384 = new SHA384CryptoServiceProvider();//建立一個SHA384
+            string resultSha384 = Convert.ToBase64String(sha384.ComputeHash(Encoding.Default.GetBytes(source)));//把加密後的字串從Byte[]轉為字串
+            return resultSha384;
+        }
+        /// <summary>
+        /// SHA512加密字串
+        /// </summary>
+        /// <param name="source">原字串</param>
+        /// <returns>加密後字串</returns>
+        public string EncodingSHA512(string source)
+        {
+            SHA512 sha512 = new SHA512CryptoServiceProvider();
+            string resultSha512 = Convert.ToBase64String(sha512.ComputeHash(Encoding.Default.GetBytes(source)));
+            return resultSha512;
+        }
+    }
+}
--- Solution/WebApplication/Services/MemberService.cs
+++ Solution/WebApplication/Services/MemberService.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebApplication.Models;
 
 namespace WebApplication.Services
 {
     /// <summary>
     /// 提供會員功能相關邏輯及資料庫操作
     /// </summary>
-    public class MemberService
+    public sealed class MemberService
     {
+        private MyForumEntities db;
+        public MemberService()
+        {
+            db = new MyForumEntities();
+        }
+
+        /// <summary>
+        /// 註冊會員
+        /// </summary>
+        /// <param name="newMember"></param>
+        public void Register(Member newMember)
+        {
+
+        }
     }
 }
