75fea052e45a3900d830604f77365ec78ffdb0f6
Ian Chen 陳立峯 <fds26875@hotmail.com>
2017-07-23 21:50:11 +0800

    新增MailService,及設定檔

---
+++ Solution/WebApplication/Services/MailService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication.Services
+{
+    public class MailService
+    {
+        private MyForumEntities db;
+
+
+
+        public MailService()
+        {
+            db = new MyForumEntities();
+        }
+    }
+}
