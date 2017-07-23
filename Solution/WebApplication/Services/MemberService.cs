f8d196b2037a2f10c65529239de52998f472044e
Ian Chen 陳立峯 <fds26875@hotmail.com>
2017-07-23 20:18:46 +0800

    新增Serivces資料夾,並建立MemberService

---
+++ Solution/WebApplication/Services/MemberService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Services
+{
+    /// <summary>
+    /// 提供會員功能相關邏輯及資料庫操作
+    /// </summary>
+    public class MemberService
+    {
+    }
+}
