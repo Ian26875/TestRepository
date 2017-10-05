3236d29e93d8776159c0c269e4a596d9d8e0297e
Ian_Chen <fds26875@hotmail.com.tw>
2017-10-05 16:19:31 +0800

    加入CustomService介面定義服務提供Method

---
+++ MVCWebNorthWind/MVCWebNorthWind/Services/Interface/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using MVCWebNorthWind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCWebNorthWind.Services.Interface
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customers> GetCustomersByCondition(string companyName,string contactName);
+        Customers AddCustomer(Customers customers);
+        Customers DeleteCustomer(Customers customers);
+        Customers EditCustomer(Customers customers);
+        Customers GetAllCustomers();
+    }
+}
