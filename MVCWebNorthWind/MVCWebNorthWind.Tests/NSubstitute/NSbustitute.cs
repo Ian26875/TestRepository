ce7695f1a7510bdbe00fede4518b22ad2949aace
Ian_Chen <fds26875@hotmail.com.tw>
2017-10-16 10:52:31 +0800

    UnitTest Project 加入NSubstitute And CustomersServiceTest 並加入空白的SpecFlow.Feature

---
+++ MVCWebNorthWind/MVCWebNorthWind.Tests/NSubstitute/NSbustitute.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NSubstitute.Exceptions;
+
+namespace MVCWebNorthWind.Tests.NSubstitute
+{
+    [TestClass]
+    public class NSbustitute
+    {
+
+        [TestMethod]
+        [Owner("Ian")]
+        public void Add_輸入參數1及2_預期結果為3()
+        {
+            //arrange
+            //建立Mock物件
+            ICalculator calculator = Substitute.For<ICalculator>();
+            //指定方法並指定回傳值
+            calculator.Add(1, 2).Returns(3);
+            int expected = 3;
+            //act
+            int actual = calculator.Add(1, 2);
+            //assert
+            Assert.AreEqual(expected, actual);
+            
+        }
+
+        [TestMethod]
+        public void Test_GetStarted_ReceivedSpecificCall()
+        {
+            ICalculator calculator = Substitute.For<ICalculator>();
+            calculator.Add(1, 2);
+            //檢查是否有接收的指定調用
+            calculator.Received().Add(1, 2);
+            calculator.DidNotReceive().Add(5, 7);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ReceivedCallsException))]
+        public void Test_GetStarted_DidNotReceivedSpecificCall()
+        {
+            ICalculator calculator = Substitute.For<ICalculator>();
+            calculator.Add(5, 7);
+
+            calculator.Received().Add(1, 2);
+        }
+
+        [TestMethod]
+        public void Test_GetStarted_MatchArguments()
+        {
+            ICalculator calculator = Substitute.For<ICalculator>();
+
+            calculator.Add(10, -5);
+
+            calculator.Received().Add(10, Arg.Any<int>());
+            calculator.Received().Add(10, Arg.Is<int>(x => x < 0));
+        }
+    }
+}
--- MVCWebNorthWind/MVCWebNorthWind.Tests/Services/CustomersServiceTest.cs
+++ MVCWebNorthWind/MVCWebNorthWind.Tests/Services/CustomersServiceTest.cs
@@ -1,15 +1,12 @@
-﻿using LinqKit;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MVCWebNorthWind.Models;
 using MVCWebNorthWind.Respositories;
 using MVCWebNorthWind.Services;
 using MVCWebNorthWind.Services.Interface;
 using MVCWebNorthWind.Tests.Mocket;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NSubstitute;
+using System.Collections.Generic;
 
 namespace MVCWebNorthWind.Tests.Services
 {
@@ -55,5 +52,26 @@ namespace MVCWebNorthWind.Tests.Services
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+
+
+        [TestMethod]
+        [Owner("Ian")]
+        public void 測試方法_測試情境_預期結果()
+        {
+            //arrange        
+            IGenerRespository<Customers> generRespository = Substitute.For<IGenerRespository<Customers>>();
+          
+            ICustomerService service = new CustomerService(generRespository);
+            string companyName = "";
+            string contactName = "";
+            var expected = 0;
+            //act
+            var customers = service.GetCustomersByCondition(companyName, contactName);
+            var actual = customers.ToList().Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
