6d6055d2f64ff8e076e3e2cc2938544e23d070bc
Ian_Chen <fds26875@hotmail.com.tw>
2017-11-28 10:11:35 +0800

    幫助類加入及單元測試加入

---
+++ MVCWebNorthWind/MVCWebNorthWind/ViewModels/PaymentInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// The ViewModels namespace.
+/// </summary>
+namespace MVCWebNorthWind.ViewModels
+{
+    public class PaymentInfo
+    {
+        /// <summary>
+        /// Gets or sets the annual percentage rate.
+        /// </summary>
+        /// <value>The annual percentage rate.</value>
+        [Display(Name = "年度百分率")]
+        public double AnnualPercentageRate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the payment details.
+        /// </summary>
+        /// <value>The payment details.</value>
+        [Display(Name = "繳款明細")]
+        public IEnumerable<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
