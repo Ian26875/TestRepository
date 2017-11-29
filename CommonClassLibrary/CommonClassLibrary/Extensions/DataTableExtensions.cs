2aa7995df5c1ae2dc640eb4001931cb671bcbc8b
Ian_Chen <fds26875@hotmail.com.tw>
2017-11-29 16:42:27 +0800

    修改DT

---
+++ CommonClassLibrary/CommonClassLibrary/Extensions/DataTableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonClassLibrary.Extensions
+{
+    /// <summary>
+    /// Class DataTableExtensions.
+    /// </summary>
+    public static class DataTableExtensions
+    {
+        /// <summary>
+        /// To the list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dataTable">The data table.</param>
+        /// <returns>List&lt;T&gt;.</returns>
+        public static List<T> ToList<T>(this DataTable dataTable) where T:class,new()
+        {
+            List<T> items = new List<T>();
+           
+            return items;
+        }
+    }
+}
---
+++ CommonClassLibrary/NUnit.Tests1/DataTableExtensionTestClass.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Xml;
+using CommonClassLibrary.Extensions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class DataTableExtensionTestClass
+    {
+        public class Test
+        {
+            public string StringColumn { get; set; }
+            public int IntColumn { get; set; }
+            public decimal DecimalColumn { get; set; }
+            public long LongColumn { get; set; }
+            public double DoubleColumn { get; set; }
+        }
+
+        private DataTable dataTableTest;
+
+        [SetUp]     
+        public void DataTableTestInit()
+        {
+            dataTableTest = new DataTable();
+            dataTableTest.Columns.Add("StringColumn", typeof(string));
+            dataTableTest.Columns.Add("IntColumn", typeof(int));
+            dataTableTest.Columns.Add("DecimalColumn", typeof(decimal));
+            dataTableTest.Columns.Add("LongColumn", typeof(long));
+            dataTableTest.Columns.Add("DoubleColumn", typeof(double));
+            DataRow dataRow = dataTableTest.NewRow();
+            dataRow.SetField("StringColumn", "TESTString");
+            dataRow.SetField("IntColumn", 100);
+            dataRow.SetField("DecimalColumn", 147852);
+            dataRow.SetField("LongColumn", 123456789);
+            dataRow.SetField("DoubleColumn", 0.123);
+            dataTableTest.Rows.Add(dataRow);
+        }
+
+
+        [Test]
+        [Category("DataTableExtension")]
+        [TestCaseSource("DataTableInit")]
+        public void ToList_將DataTable轉型成List強型別物件_內容資料必須完全相同()
+        {
+            //arrange
+
+            var test = new Test
+            {
+                StringColumn="",
+                IntColumn= 100,
+                DecimalColumn= 147852,
+                LongColumn= 123456789,
+                DoubleColumn= 0.123
+            };
+            List<Test> expected = new List<Test>() { test };
+            //act
+            var actual = dataTableTest.ToList<Test>();
+            
+            //assert
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+
+       
+    }
+}
