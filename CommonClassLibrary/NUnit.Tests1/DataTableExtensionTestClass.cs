071e8b5f4c653eddf0efde46eb7f3157767279d2
Ian_Chen <fds26875@hotmail.com.tw>
2017-11-30 19:18:20 +0800

    新增測試案例,當目標只為空資料列

--- CommonClassLibrary/NUnit.Tests1/DataTableExtensionTestClass.cs
+++ CommonClassLibrary/NUnit.Tests1/DataTableExtensionTestClass.cs
@@ -13,43 +13,36 @@ namespace NUnit.Tests1
     [TestFixture]
     public class DataTableExtensionTestClass
     {
-        public class Test
-        {
-            public string StringColumn { get; set; }
-            public int IntColumn { get; set; }
-            public decimal DecimalColumn { get; set; }
-            public long LongColumn { get; set; }
-            public double DoubleColumn { get; set; }
-            public override bool Equals(object obj)
-            {
-                var input = obj as Test;
-                if (input == null)
-                {
-                    return false;
-                }
-                else
-                {
-                    var result = StringColumn == input.StringColumn
-                        && IntColumn == input.IntColumn
-                         && DecimalColumn == input.DecimalColumn
-                           && LongColumn == input.LongColumn
-                             && DoubleColumn == input.DoubleColumn;
-                    return result;
-                }
-            }
-        }
+        private DataTable dataTableTest;
 
-        [Test]
-        [Category("DataTableExtension")]
-        public void ToList_將DataTable轉型成List強型別物件_內容資料必須完全相同()
+        [SetUp]
+        public void Init()
         {
-            //arrange
-            DataTable dataTableTest = new DataTable();
+            dataTableTest = new DataTable();
             dataTableTest.Columns.Add("StringColumn", typeof(string));
             dataTableTest.Columns.Add("IntColumn", typeof(int));
             dataTableTest.Columns.Add("DecimalColumn", typeof(decimal));
             dataTableTest.Columns.Add("LongColumn", typeof(long));
             dataTableTest.Columns.Add("DoubleColumn", typeof(double));
+           
+        }
+        [Test]
+        [Category("DataTableExtension")]
+        public void ToList_將資料列為空DataTable轉型成List強型別物件_List資料為空但不能為空物件()
+        {
+            //arrange       
+            List<Test> expected = new List<Test>();
+            //act
+            List<Test> actual = dataTableTest.ToList<Test>();
+            //assert
+            Assert.That(actual, Is.EquivalentTo(expected));
+
+        }
+        [Test]
+        [Category("DataTableExtension")]
+        public void ToList_將DataTable轉型成List強型別物件_內容資料必須完全相同()
+        {
+            //arrange
             DataRow dataRow = dataTableTest.NewRow();
             dataRow.SetField("StringColumn", "TEST");
             dataRow.SetField<int>("IntColumn", 123);
@@ -57,6 +50,7 @@ namespace NUnit.Tests1
             dataRow.SetField<long>("LongColumn", 123456789);
             dataRow.SetField<double>("DoubleColumn", 0.123);
             dataTableTest.Rows.Add(dataRow);
+
             List<Test> expected = new List<Test>() {  new Test
             {
                 StringColumn = "TEST",
@@ -71,6 +65,31 @@ namespace NUnit.Tests1
             //assert
             Assert.That(actual,Is.EquivalentTo(expected));
    
+        }
+        public class Test
+        {
+            public string StringColumn { get; set; }
+            public int IntColumn { get; set; }
+            public decimal DecimalColumn { get; set; }
+            public long LongColumn { get; set; }
+            public double DoubleColumn { get; set; }
+            public override bool Equals(object obj)
+            {
+                Test input = obj as Test;
+                if (input == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    bool result = StringColumn == input.StringColumn
+                        && IntColumn == input.IntColumn
+                        && DecimalColumn == input.DecimalColumn
+                        && LongColumn == input.LongColumn
+                        && DoubleColumn == input.DoubleColumn;
+                    return result;
+                }
+            }
         }
     }
 
